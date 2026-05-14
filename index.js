29e5776f40696da33a6938aaacad79009bf25ef1
farhattyagi8-del <farhattyagi8@gmail.com>
2026-05-14 15:55:33 +0530

    add change

--- index.js
+++ index.js
@@ -127,6 +127,21 @@ boxes.forEach((box) => {    //this use for add event listner on each box when cl
 
  const showWinner = (winner) => { // this function use for show the winner message when there is a winner and also disable all the boxes
     msg.innerText = `Congratulations! ${winner} wins!`;
+    msg.style.position = "fixed";
+      msg.style.top = "49%";
+        msg.style.left = "50%";
+        
+            msg.style.transform = "translate(-50%, -50%)";
+            msg.style.fontSize = "4rem";
+             msg.style.fontStyle = "italic";
+              msg.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+               msg.style.padding = "20px 40px";
+                msg.style.borderRadius = "10px";
+                msg.style.border = "2px solid rgb(246, 219, 44)";
+             msg.style.color = "rgb(246, 219, 44)";
+              msg.style.textShadow = "0 0 10px rgba(255, 255, 255, 0.8)";
+               msg.style.textShadow = "0 0 10px rgba(255, 255, 255, 0.6)";
+                msg.style.textShadow = "0 0 10px rgba(255, 255, 255, 0.4)";
     if (!isMuted) winSound.play();
     showConfetti();
     msgContainer.classList.remove("hidden");
